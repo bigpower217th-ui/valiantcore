504739f69a1776a02079d4b24ce22be7b99a1976
C/MASTER <bigpower217th@gmail.com>
2026-06-16 13:42:58 +0300

    pit.c > pic.c > idt.c > function > kmain.c

---
+++ kernel/kmain.c
@@ -0,0 +1,47 @@
+/*
+ * ValiantCore Kernel
+ * Copyright (C) 2026 bigpower
+ *
+ * This program is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+#include "../include/kernel.h"
+#include "../include/scheduler.h"
+
+#define kprint_banner() kprint("--- ValiantCore OS Loading ---\n")
+extern void vfs_init();
+extern int rtl8111_init();
+extern void net_init();
+
+
+void kmain() {
+
+   init_gdt();     // By BigPower
+   pic_init();     // 
+   pit_init(1000); // 
+   init_idt();     //
+   init_scheduler(); 
+
+   kprint_banner();
+
+   monitor_system_integrity();
+ 
+   vfs_init();
+
+
+   net_init();
+   if (rtl8111_init() == 0) {
+      kprint("[KERNEL] RTL8111 driver loaded.\n");
+          } else {
+              kprint("[KERNEL] ERROR: RTL8111 driver failed!\n");
+          }
+
+          kprint("[KERNEL] ValiantCore ready.\n");
+
+              while (1);
+              }
